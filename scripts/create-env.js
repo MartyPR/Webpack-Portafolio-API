aee3eee308afae32fbf545bf9893eb84d64831c4
MartyPR <martin.p1131@gmail.com>
2023-02-01 18:04:38 -0500

    webserver, analyzer

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
 fs.writeFileSync('./.env', `API=${process.env.API}\n`)
+
+//  "build": " node ./scripts/create-env.js && webpack --mode production --config webpack.config.js",
